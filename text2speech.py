6cf18303c9a4905bb1b176afddb7894a7263760f
vanthoai <supernovavanthoai@gmail.com>
2020-08-13 14:50:49 +0100

    "text to speech rest of code"

--- text2speech.py
+++ text2speech.py
@@ -1,1 +1,25 @@
 import pyttsx3
+import tkinter as tk
+def tts():
+    engine = pyttsx3.init()
+    engine.say(entry.get())
+    engine.runAndWait()
+
+root = tk.Tk()
+root.title("Text2Speech Machine")
+# row 0
+frame = tk.Frame(root)
+frame.grid(row=0)
+# row 1
+instruction = tk.Label(root, text="Enter the sentence you would to be read out:")
+instruction.grid(row=1)
+# row 2
+entry = tk.Entry(root, width=50)
+entry.grid(row=2)
+# row 3
+submit = tk.Button(root, text="Submit", command=tts)
+submit.grid(row=3)
+
+
+root.mainloop()
+
